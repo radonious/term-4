21ebd2be5577713b14ee24b0fae774f6a0db8ec1
Alexander <67727902+radonious@users.noreply.github.com>
2023-04-19 17:11:25 +0700

    Delete lab4 directory

--- lab4/src/main/java/com/example/lab1/BaseAI.java
+++
@@ -1,7 +0,0 @@
-package com.example.lab1;
-
-public abstract class BaseAI implements Runnable {
-    @Override public void run() {}
-
-    public synchronized void move() {}
-}
--- lab4/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.lab1 to javafx.fxml;
-    exports com.example.lab1;
-}
